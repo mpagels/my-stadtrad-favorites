7eb3ef104c4a5aeb8eb4717243ac3f1a9ea2bd8f
Martin <office@martinpagels.de>
2021-08-30 12:32:50 +0200

    Disable eslint warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom'
 import FooterNavBar from './components/FooterNavBar'
 import Header from './components/Header'
+// eslint-disable-next-line no-unused-vars
 import Stadtrad from './components/Stadtrad'
 import Test from './components/Test'
 import Thing from './pages/Thing'
