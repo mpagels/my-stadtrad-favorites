64623aa8f4088d0592e95523d413faa0fc4ac23c
Martin Pagels <office@martinpagels.de>
2021-08-31 17:18:32 +0200

    Working prroxi in apiFetch now

--- src/services/apiFetch.js
+++ src/services/apiFetch.js
@@ -1,13 +1,9 @@
 export default function fetchLocations() {
-  return fetch('http://localhost:5000/api/get-location').then(function (
-    response
-  ) {
+  return fetch('/api/get-location').then(function (response) {
     return response.json()
   })
 }
 
 export function fetchThing(id) {
-  return fetch(`http://localhost:5000/api/get-thing/${id}`).then((response) =>
-    response.json()
-  )
+  return fetch(`/api/get-thing/${id}`).then((response) => response.json())
 }
