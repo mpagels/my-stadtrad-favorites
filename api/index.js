2116b14470e9d667fb66b8f156a4d01e163f9522
Martin <office@martinpagels.de>
2021-09-01 01:27:59 +0200

    Add api file for serverless express vercel
    - https://vercel.com/guides/using-express-with-vercel

---
+++ api/index.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+//const port = process.env.PORT || 5000
+const fetch = require('node-fetch')
+//const cors = require('cors')
+var dayjs = require('dayjs')
+
+//pp.use(cors())
+
+//app.listen(port, () => console.log(`Listening on port ${port}`))
+
+app.get('/api/get-location', async (req, res) => {
+  const locations = await Promise.all([
+    fetch(
+      'https://iot.hamburg.de/v1.0/Things?$filter=Datastreams/properties/serviceName%20eq%20%27HH_STA_StadtRad%27&$orderby=id&$count=true&$expand=Locations'
+    ),
+    fetch(
+      'https://iot.hamburg.de/v1.0/Things?$skip=100&$filter=%28Datastreams%2Fproperties%2FserviceName+eq+%27HH_STA_StadtRad%27%29&$expand=Locations&$orderby=id+asc&$count=true'
+    ),
+    fetch(
+      'https://iot.hamburg.de/v1.0/Things?$skip=200&$filter=%28Datastreams%2Fproperties%2FserviceName+eq+%27HH_STA_StadtRad%27%29&$expand=Locations&$orderby=id+asc&$count=true'
+    ),
+  ]).then(function (responses) {
+    // Get a JSON object from each of the responses
+    return Promise.all(
+      responses.map(function (response) {
+        return response.json()
+      })
+    )
+  })
+  const flattenLocations = locations.map((location) => location.value).flat()
+  const response = flattenLocations.map((location) => {
+    return {
+      station_description: location.description,
+      thing_id: location['@iot.id'],
+    }
+  })
+
+  res.status(200).json(response)
+})
+
+app.get('/api/get-thing/:id', async (req, res) => {
+  const { id } = req.params
+
+  const thing = await fetch(
+    `https://iot.hamburg.de/v1.0/Things(${id})/Datastreams`
+  ).then((res) => res.json())
+
+  const dataStream_id = thing.value[0]['@iot.id']
+
+  const getThingName = await fetch(
+    thing.value[0]['Thing@iot.navigationLink']
+  ).then((res) => res.json())
+
+  const getAvailablesBikes = await fetch(
+    `https://iot.hamburg.de/v1.0/Datastreams(${dataStream_id})/Observations?$top=2&$select=phenomenonTime,result&$orderby=phenomenonTime+desc`
+  ).then((res) => res.json())
+
+  const availableBikes = getAvailablesBikes.value[0].result
+  const lastUpdated = dayjs(getAvailablesBikes.value[0].phenomenonTime).format(
+    'DD.MM. - HH:mm:ss'
+  )
+
+  const [lat, long] = thing.value[0].observedArea.coordinates[1]
+
+  const response = {
+    station_description: getThingName.description,
+    coordinates: [long, lat],
+    dataStream_id,
+    availableBikes,
+    lastUpdated,
+    thing_id: id,
+  }
+  res.status(200).json(response)
+})
+
+module.exports = app
