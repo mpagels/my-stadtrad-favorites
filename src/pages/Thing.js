e0eea910645dfaa409c02c51e57efb238b3d3ab1
Martin <office@martinpagels.de>
2021-09-02 23:13:59 +0200

    Refactor Map
    - use new Map component

--- src/pages/Thing.js
+++ src/pages/Thing.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import Map from '../components/Map'
 import { useQuery } from 'react-query'
 import { useParams, useHistory } from 'react-router-dom'
 import styled from 'styled-components'
@@ -41,23 +41,13 @@ export default function Thing({ toggleFavorit, favorites }) {
   return (
     <Wrapper>
       <Blue />
-
-      <MyMap
-        center={[coordinates[0], coordinates[1]]}
-        zoom={18}
-        scrollWheelZoom={true}
+      <Map
+        coordinates={[coordinates[0], coordinates[1]]}
+        zoomNumber={18}
         height="300px"
         width="90%"
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[coordinates[0], coordinates[1]]}>
-          <Popup>Stadtrad-Station</Popup>
-        </Marker>
-        <ChangeMapView coords={[coordinates[0], coordinates[1]]} />
-      </MyMap>
+        hasShadow={true}
+      ></Map>
       <StationInfos>
         <Title>{title}</Title>
         <LastUpdated>
@@ -79,27 +69,6 @@ export default function Thing({ toggleFavorit, favorites }) {
     </Wrapper>
   )
 }
-
-function ChangeMapView({ coords }) {
-  const map = useMap()
-  map.setView(coords, map.getZoom())
-
-  return null
-}
-
-const MyMap = styled(MapContainer)`
-  /*
-    Any dynamic styling that will change the
-    dynamically generated classname will remove
-    the leaflet classnames from the container.
-  */
-  border-radius: 15px;
-  height: ${(props) => props.height};
-  width: ${(props) => props.width ?? '100vw'};
-  box-shadow: 0 5.9px 5.4px -84px rgba(0, 0, 0, 0.133),
-    0 19.7px 18.1px -84px rgba(0, 0, 0, 0.197),
-    0 88px 81px -84px rgba(0, 0, 0, 0.33);
-`
 
 const BackButton = styled.button`
   all: unset;
