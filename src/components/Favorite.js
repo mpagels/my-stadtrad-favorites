8f259ab64b6c6b578f03f12908a654775789b7c9
Martin <office@martinpagels.de>
2021-09-07 00:50:52 +0200

    Make map in Favorite smaller, and available bikes bold

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -35,7 +35,7 @@ export default function Favorite({ thing_id, toggleFavorit }) {
       <Map
         coordinates={coordinates}
         zoomNumber={15}
-        height="150px"
+        height="110px"
         width={'auto'}
         zoomControl={false}
         hasZIndex={true}
@@ -55,6 +55,7 @@ const Available = styled.p`
   text-align: center;
   font-size: 2em;
   margin: 0;
+  font-weight: 800;
 `
 
 const ButtonWrapper = styled.div`
@@ -70,7 +71,6 @@ const FavoriteWrapper = styled.section`
     isAvailable ? '#BDE7BD' : '#FFB6B3'};
   padding: 5px;
   border-radius: 15px;
-  height: 300px;
   box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.02),
     0 6.7px 5.3px rgba(0, 0, 0, 0.028), 0 12.5px 10px rgba(0, 0, 0, 0.035),
     0 22.3px 17.9px rgba(0, 0, 0, 0.042), 0 41.8px 33.4px rgba(0, 0, 0, 0.05),
