3c2667179a7da95d42c4838f84692fd88f7b9b82
Martin <office@martinpagels.de>
2021-09-07 01:21:07 +0200

    Make Header showing title depending on path

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 
 export default function Header() {
-  return <HeaderWrapper>Meine Stadtrad Stationen</HeaderWrapper>
+  return (
+    <Switch>
+      <Route exact path="/">
+        <HeaderWrapper>Meine Favoriten</HeaderWrapper>
+      </Route>
+      <Route path="/search">
+        <HeaderWrapper>Stationssuche</HeaderWrapper>
+      </Route>
+      <Route path="/thing">
+        <HeaderWrapper>Mein Favorit</HeaderWrapper>
+      </Route>
+    </Switch>
+  )
 }
 
 const HeaderWrapper = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
+  color: white;
+  font-size: 1.2em;
+  background-color: #003063;
 `
