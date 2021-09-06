2cf9c119aad2725023d96a156c1f8e75bed76f3a
Martin <office@martinpagels.de>
2021-09-07 00:58:02 +0200

    Add shadow to RefreshButton

--- src/components/FooterNavBar/FooterNavBar.js
+++ src/components/FooterNavBar/FooterNavBar.js
@@ -120,6 +120,12 @@ const RefreshButton = styled.button`
   align-items: center;
   padding: 15px;
   background-color: white;
+  box-shadow: 0 0px 2.2px -20px rgba(0, 0, 0, 0.121),
+    0 0px 5.3px -20px rgba(0, 0, 0, 0.174),
+    0 0px 10px -20px rgba(0, 0, 0, 0.215),
+    0 0px 17.9px -20px rgba(0, 0, 0, 0.256),
+    0 0px 33.4px -20px rgba(0, 0, 0, 0.309),
+    0 0px 80px -20px rgba(0, 0, 0, 0.43);
 `
 
 const rotation = keyframes`
