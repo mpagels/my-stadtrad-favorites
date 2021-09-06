48fd908723e4f55d76c2b3e87e5a18f182c2703e
Martin <office@martinpagels.de>
2021-09-06 22:45:24 +0200

    Add shadow to FooterNavBar

--- src/components/FooterNavBar/FooterNavBar.js
+++ src/components/FooterNavBar/FooterNavBar.js
@@ -21,12 +21,19 @@ export default function FooterNavBar() {
 const Footer = styled.footer`
   position: fixed;
   bottom: 0px;
+  right: 0px;
   height: 75px;
   width: 100vw;
   background-color: white;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  box-shadow: 0 0px 2.2px -20px rgba(0, 0, 0, 0.121),
+    0 0px 5.3px -20px rgba(0, 0, 0, 0.174),
+    0 0px 10px -20px rgba(0, 0, 0, 0.215),
+    0 0px 17.9px -20px rgba(0, 0, 0, 0.256),
+    0 0px 33.4px -20px rgba(0, 0, 0, 0.309),
+    0 0px 80px -20px rgba(0, 0, 0, 0.43);
 `
 const Navigation = styled.nav`
   width: 100%;
