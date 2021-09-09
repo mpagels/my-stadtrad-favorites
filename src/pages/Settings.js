73321f97ee18581da1d1da55b2c2cbbc28cc849f
Martin <office@martinpagels.de>
2021-09-10 01:42:38 +0200

    Add Settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,5 +1,21 @@
 import React from 'react'
+import Love from '../components/Love'
+import styled from 'styled-components'
+import OtherInfo from '../components/Setting/OtherInfo'
+import General from '../components/Setting/General'
 
 export default function Settings() {
-  return <div>Hello World</div>
+  return (
+    <PageWrapper>
+      <General />
+      <OtherInfo />
+      <Love />
+    </PageWrapper>
+  )
 }
+
+const PageWrapper = styled.div`
+  background-color: #003063;
+  color: white;
+  padding: 20px 20px 140px 20px;
+`
