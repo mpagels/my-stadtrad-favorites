952d790372956d7868edb3fc46d80c0f3e361731
Martin Pagels <office@martinpagels.de>
2021-10-30 19:43:19 +0200

    Add i18n provider to index

--- src/index.js
+++ src/index.js
@@ -5,15 +5,27 @@ import reportWebVitals from './reportWebVitals'
 import GlobalStyles from './GlobalStyles'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { i18n } from '@lingui/core'
+import { I18nProvider } from '@lingui/react'
+import { messages as enMessages } from './locales/en/messages'
+import { messages as deMessages } from './locales/de/messages'
 
 const queryClient = new QueryClient()
+
+i18n.load({
+  en: enMessages,
+  de: deMessages,
+})
+i18n.activate('en')
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
         <GlobalStyles />
-        <App />
+        <I18nProvider i18n={i18n}>
+          <App />
+        </I18nProvider>
       </QueryClientProvider>
     </Router>
   </React.StrictMode>,
